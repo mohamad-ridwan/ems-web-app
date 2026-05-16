e06b8c1bd2aafd311f11a05efa95ebf307843ae6
mohamad-ridwan <mr643062@gmail.com>
2026-05-17 02:52:46 +0700

    feat: create shared-theme library and migrate application styles to central SASS configuration

--- apps/ems_login/src/app/login/login.component.ts
+++ apps/ems_login/src/app/login/login.component.ts
@@ -9,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   private http = inject(HttpClient);
---
+++ libs/shared-theme/src/lib/shared-theme.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-shared-theme',
+  imports: [],
+  templateUrl: './shared-theme.html',
+  styleUrl: './shared-theme.css',
+})
+export class SharedTheme {}
