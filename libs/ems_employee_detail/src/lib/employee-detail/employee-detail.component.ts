787e7ffaf53fb3bd329eda2b706be9e5fb76fbac
mohamad-ridwan <mr643062@gmail.com>
2026-05-17 17:22:44 +0700

    refactor: inline employee-detail UI component and remove unused nx-welcome file

--- libs/ems_employee_detail/src/lib/employee-detail/employee-detail.component.ts
+++ libs/ems_employee_detail/src/lib/employee-detail/employee-detail.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EmployeeDetailUiComponent } from '../ui/employee-detail-ui.component';
 import { EmployeeDetailFacade } from './facade/employee-detail.facade';
+import { ButtonComponent, ButtonIconComponent } from '@org/ui-kit';
 
 @Component({
   selector: 'app-employee-detail',
   standalone: true,
-  imports: [CommonModule, EmployeeDetailUiComponent],
+  imports: [CommonModule, ButtonComponent, ButtonIconComponent],
   providers: [EmployeeDetailFacade],
-  templateUrl: './employee-detail.view.html'
+  templateUrl: './employee-detail.view.html',
+  styleUrls: ['./employee-detail.component.scss']
 })
 export class EmployeeDetailComponent implements OnInit {
   public facade = inject(EmployeeDetailFacade);
 
   ngOnInit(): void {
     this.facade.init();
+  }
+
+  formatCurrency(value: number): string {
+    return new Intl.NumberFormat('id-ID', {
+      style: 'currency',
+      currency: 'IDR',
+      minimumFractionDigits: 2
+    }).format(value).replace('IDR', 'Rp.');
   }
 }
