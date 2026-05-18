661bcab09c10ae771253eb5843c9e5096daa32b9
mohamad-ridwan <mr643062@gmail.com>
2026-05-18 14:03:02 +0700

    refactor: update import path for EmsLoginRootComponent in index.ts

--- libs/ems_login/src/index.ts
+++ libs/ems_login/src/index.ts
@@ -1,2 +1,2 @@
 export { LoginComponent } from './lib/form/login.component';
-export { EmsLoginRootComponent } from './lib/app/app.component';
+export { EmsLoginRootComponent } from './app/app.component';
